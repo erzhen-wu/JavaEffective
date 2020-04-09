2041345aad8519281988a24ae9ae1f6c525b717e
Dave1379 <1160115913@qq.com>
2020-04-09 23:41:28 +0800

    3

--- src/main/java/com/github/ch2/Pizza.java
+++ src/main/java/com/github/ch2/Pizza.java
@@ -1,4 +1,20 @@
 package com.github.ch2;
 
+
+import org.omg.CORBA.Object;
+
+import java.util.EnumSet;
+import java.util.Set;
+
 public abstract class Pizza {
+    //构建一个枚举值
+    public enum Topping {HAM, MUSHROOM, ONION, PEPPER, SAUSAGE}
+/*    final Set<Topping> topping;
+    abstract static class Builder<T extends Builder<T>>{
+        EnumSet<Topping> toppings =EnumSet.noneOf(Topping.class);
+        public T addTopping(Topping topping){
+            toppings.add(Objects)
+        }
+    }*/
+
 }
